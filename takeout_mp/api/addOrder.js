1397a7da466b8dd347329e62059e250b7acaf860
Anesidora <104627589+anetigone@users.noreply.github.com>
2025-05-11 16:54:11 +0800

    把前端分出去了

--- takeout_mp/api/addOrder.js
+++
@@ -1,9 +0,0 @@
-
-import regeneratorRuntime, { async } from '../lib/runtime/runtime';
-
-export const addOrderApi =(data) =>{
-	return uni.$ajax.post({
-		url: 'mp/orders/submit',
-		data: data
-	})
-}
--- takeout_mp/api/sendCode.js
+++
@@ -1,9 +0,0 @@
-
-import regeneratorRuntime, { async } from '../lib/runtime/runtime';
-
-export const updateUserInfo = (data) =>{
-	return uni.$ajax.put({
-		url: 'mp/user/updateUserInfo',
-		data: data
-	})
-}
